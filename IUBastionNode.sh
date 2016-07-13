f27b88c12a0971d979e1b02d58a3cd8917157815
Steven Roach <stevenwroach@hotmail.com>
2016-07-13 17:16:54 -0400

    Update IUBastionNode.sh

--- IUBastionNode.sh
+++ IUBastionNode.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo "export IU_APP_NAME="$1 >>/etc/profile.d/iuinit.sh
-echo "export IU_NODE_TYPE=bastion" >>/etc/profile.d/iuinit.sh
-echo "export IU_AGENT_PROFILE="$2 >>/etc/profile.d/iuinit.sh
-echo "export SSH_KEY_PUB_URL="$3 >>/etc/profile.d/iuinit.sh
-echo "export SSH_KEY_PRIV_URL="$4 >>/etc/profile.d/iuinit.sh
-source /etc/profile.d/iuinit.sh
-echo "IU_APP_NAME="$1 >>/etc/environment
-echo "IU_NODE_TYPE=bastion" >>/etc/environment 
-echo "IU_AGENT_PROFILE="$2 >>/etc/environment
-echo "IU_NODE_TYPE=database" >>/etc/environment
-echo "SSH_KEY_PUB_URL="$3 >>/etc/environment
-echo "SSH_KEY_PRIV_URL="$4 >>/etc/environment
+#echo "export IU_APP_NAME="$1 >>/etc/profile.d/iuinit.sh
+#echo "export IU_NODE_TYPE=bastion" >>/etc/profile.d/iuinit.sh
+#echo "export IU_AGENT_PROFILE="$2 >>/etc/profile.d/iuinit.sh
+#echo "export SSH_KEY_PUB_URL="$3 >>/etc/profile.d/iuinit.sh
+#echo "export SSH_KEY_PRIV_URL="$4 >>/etc/profile.d/iuinit.sh
+#source /etc/profile.d/iuinit.sh
+#echo "IU_APP_NAME="$1 >>/etc/environment
+#echo "IU_NODE_TYPE=bastion" >>/etc/environment 
+#echo "IU_AGENT_PROFILE="$2 >>/etc/environment
+#echo "IU_NODE_TYPE=database" >>/etc/environment
+#echo "SSH_KEY_PUB_URL="$3 >>/etc/environment
+#echo "SSH_KEY_PRIV_URL="$4 >>/etc/environment
 # iu-aws-agent --operation get_ssh_key
 # curl -H "Content-Type: application/json" -X POST -d '{"existing_domain":"altairunlimited.com", "new_domain":"",
 #	{ "Fn::GetAtt" : [ "IUFrontendLoadBalancer", "DNSName" ]},"\"}' http://10.0.0.20:8000/iu-update/appserver/config", "\n",
