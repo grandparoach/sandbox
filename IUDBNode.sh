5448b5606d3e8e3835978316037dcdd338545c6d
Steven Roach <stevenwroach@hotmail.com>
2016-07-13 17:17:52 -0400

    Update IUDBNode.sh

--- IUDBNode.sh
+++ IUDBNode.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo "export IU_APP_NAME="$1 >>/etc/profile.d/iuinit.sh
-echo "export IU_NODE_TYPE=app_server" >>/etc/profile.d/iuinit.sh
-echo "export IU_AGENT_PROFILE="$2 >>/etc/profile.d/iuinit.sh
-echo "export SSH_KEY_PUB_URL="$3 >>/etc/profile.d/iuinit.sh
-echo "export SSH_KEY_PRIV_URL="$4 >>/etc/profile.d/iuinit.sh
-source /etc/profile.d/iuinit.sh
-echo "IU_APP_NAME="$1 >>/etc/environment
-echo "IU_AGENT_PROFILE="$2 >>/etc/environment
-echo "IU_NODE_TYPE=database" >>/etc/environment
-echo "SSH_KEY_PUB_URL="$3 >>/etc/environment
-echo "SSH_KEY_PRIV_URL="$4 >>/etc/environment
+#echo "export IU_APP_NAME="$1 >>/etc/profile.d/iuinit.sh
+#echo "export IU_NODE_TYPE=app_server" >>/etc/profile.d/iuinit.sh
+#echo "export IU_AGENT_PROFILE="$2 >>/etc/profile.d/iuinit.sh
+#echo "export SSH_KEY_PUB_URL="$3 >>/etc/profile.d/iuinit.sh
+##echo "export SSH_KEY_PRIV_URL="$4 >>/etc/profile.d/iuinit.sh
+#source /etc/profile.d/iuinit.sh
+#echo "IU_APP_NAME="$1 >>/etc/environment
+#echo "IU_AGENT_PROFILE="$2 >>/etc/environment
+#echo "IU_NODE_TYPE=database" >>/etc/environment
+#echo "SSH_KEY_PUB_URL="$3 >>/etc/environment
+#echo "SSH_KEY_PRIV_URL="$4 >>/etc/environment
 # iu-aws-agent --operation get_single_component --iu-component database --launch-region us-east-1 --iu-version "$5
 # iu-aws-agent --operation get_ssh_key"
