a5186b0972fb2828694a13513f61b8d5a547f8ab
Steven Roach <stevenwroach@hotmail.com>
2019-05-16 13:42:57 -0500

    Update formatopt.sh

--- formatopt.sh
+++ formatopt.sh
@@ -1,1 +1,20 @@
-ahahahs
+#!/bin/bash
+set -x
+
+# format the d drive
+fdisk /dev/sdd << EOF
+n
+p
+1
+
+
+p
+w
+EOF
+
+# make the file system
+mkfs -t xfs /dev/sdd1
+
+# mount over /opt
+echo "/dev/sdd1 /opt xfs defaults,nofail 0 2" >> /etc/fstab
+    mount /dev/sdd1
