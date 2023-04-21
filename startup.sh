028a1872dae3a1791e9361e6a838499749b70078
Steven Roach <stevenwroach@hotmail.com>
2023-04-20 18:36:22 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -16,11 +16,6 @@ fi
 sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 3600/I' /etc/ssh/sshd_config
 systemctl restart sshd
 
-#set a password for root
-echo "root:$adminPassword" | chpasswd
-
-sleep 30
-
 # Disable tty requirement for sudo
 sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
 
@@ -31,19 +26,25 @@ echo "$adminUserName ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 configure_ssh() {
     
-
-    yum -y install sshpass
-    ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -P ''
-
-    sshpass -p $adminPassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no -o ConnectTimeout=2 root@head -p 22
-    sshpass -p $adminPassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no -o ConnectTimeout=2 root@compute1 -p 22
-    sshpass -p $adminPassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no -o ConnectTimeout=2 root@compute2 -p 22
-    sshpass -p $adminPassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no -o ConnectTimeout=2 root@compute3 -p 22
-    sshpass -p $adminPassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no -o ConnectTimeout=2 root@compute4 -p 22
-       
-}
 if [ $OSselection -eq 'Rocky_8' ]
 then
-    configure_ssh
+    yum -y install sshpass
+else
+    apt-get -y install sshpass
+fi
+    ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -P ''
+
+    sshpass -p $adminPassword ssh-copy-id -i /home/$adminUserName/.ssh/id_rsa -o StrictHostKeyChecking=no -o ConnectTimeout=2 $adminUserName@head -p 22
+    sshpass -p $adminPassword ssh-copy-id -i /home/$adminUserName/.ssh/id_rsa -o StrictHostKeyChecking=no -o ConnectTimeout=2 $adminUserName@compute1 -p 22
+    sshpass -p $adminPassword ssh-copy-id -i /home/$adminUserName/.ssh/id_rsa -o StrictHostKeyChecking=no -o ConnectTimeout=2 $adminUserName@compute2 -p 22
+    sshpass -p $adminPassword ssh-copy-id -i /home/$adminUserName/.ssh/id_rsa -o StrictHostKeyChecking=no -o ConnectTimeout=2 $adminUserName@compute3 -p 22
+    sshpass -p $adminPassword ssh-copy-id -i /home/$adminUserName/.ssh/id_rsa -o StrictHostKeyChecking=no -o ConnectTimeout=2 $adminUserName@compute4 -p 22
+       
+}
+
+configure_ssh
+
+if [ $OSselection -eq 'Rocky_8' ]
+then
     yum -y update
 fi
