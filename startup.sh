06f64394ad4ef365b8a192f0756d5e3151a22a26
Steven Roach <stevenwroach@hotmail.com>
2023-04-20 18:10:31 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,10 +3,14 @@ set -x
 
 adminUserName=${1}
 adminPassword=${2}
+OSselection=${3}
 
-#disable_selinux
-sed -i 's/^SELINUX=.*/SELINUX=disabled/I' /etc/selinux/config
-setenforce 0
+if [ $OSselection -eq 'Rocky_8' ]
+then
+    #disable_selinux
+    sed -i 's/^SELINUX=.*/SELINUX=disabled/I' /etc/selinux/config
+    setenforce 0
+fi
 
 #prevent the inactive sessions from locking up
 sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 3600/I' /etc/ssh/sshd_config
@@ -38,6 +42,8 @@ configure_ssh() {
     sshpass -p $adminPassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no -o ConnectTimeout=2 root@compute4 -p 22
        
 }
-
-configure_ssh
-yum -y update
+if [ $OSselection -eq 'Rocky_8' ]
+then
+    configure_ssh
+    yum -y update
+fi
