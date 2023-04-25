ce8b9773b02df08d8331ad59e7c1083d131cc95a
Steven Roach <stevenwroach@hotmail.com>
2023-04-25 12:12:29 -0700

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,11 @@
 
 count=${1}
 end_count=${2}
+OSselection=${3}
 index=$count
 while [ $index -le $end_count ]
 do
    az group create -l southcentralus -n "student-$index"
-   az group deployment create -g "student-$index" --template-uri https://raw.githubusercontent.com/grandparoach/sandbox/LCI/azuredeploy.json --parameters @lciparams.json &
+   az group deployment create -g "student-$index" --template-uri https://raw.githubusercontent.com/grandparoach/sandbox/LCI2023/azuredeploy.json --parameters @lciparams.json OSSelection=$OSselection &
    let index=$index+1
 done
